8998e7601816ad7933bee735f788310a6efb56ed
saikumar <saikumarpayyavula145@gmail.com>
2024-05-26 16:07:22 -0500

    1389. Create Target Array in the Given Order

--- src/Arrays/L1389.java
+++ src/Arrays/L1389.java
@@ -1,7 +1,28 @@
 package Arrays;
 
 import java.util.*;
-
+/**
+ *  1389. Create Target Array in the Given Order
+ *
+ *Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+ *
+ *     Initially target array is empty.
+ *     From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+ *     Repeat the previous step until there are no elements to read in nums and index.
+ *
+ * Return the target array.
+ *
+ * It is guaranteed that the insertion operations will be valid.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+ * Output: [0,4,1,3,2]
+ *
+ *
+ * */
 public class L1389 {
     public static void main(String[] args) {
         int[] nums = {0,1,2,3,4};
