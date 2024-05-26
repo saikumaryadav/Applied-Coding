04a973c098e25a185fd15bc55f224cb2882ba26d
saikumar <saikumarpayyavula145@gmail.com>
2024-05-26 15:57:56 -0500

    905. Sort Array By Parity

--- src/Arrays/L905.java
+++ src/Arrays/L905.java
@@ -3,6 +3,8 @@ package Arrays;
 import java.util.Arrays;
 
 /**
+ * 905. Sort Array By Parity
+
  * Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
  *
  * Return any array that satisfies this condition.
